cbad8130b16add3f0245eaa05e599c3fe54e52d6
Robin CASSAGNE <robin.cassagne@wanadoo.fr>
2024-10-19 13:39:30 +0200

    Fixing longhorn issue by disabling iptable

--- Goblin-2/configuration.nix
+++ Goblin-2/configuration.nix
@@ -89,7 +89,7 @@
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
   # List services that you want to enable:
 
 ######################### Accounts #########################
