8fa9e2c101fb3221fb569f7ef182a66bdcbf9098
Robin CASSAGNE <robin.jean.cassagne@gmail.com>
2025-06-07 22:12:25 +0200

    :wrench: added grub configuration

--- common/system/generals.nix
+++ common/system/generals.nix
@@ -3,17 +3,37 @@
   pkgs,
   lib,
   ...
-}: {
+}: let
+  themeCyberpunk = pkgs.fetchFromGitHub {
+    owner = "anoopmsivadas";
+    repo = "Cyberpunk-GRUB-Theme";
+    rev = "1efd2cd4a1f82e5809e494d1c8b7b31fdbd1f3d0";
+    sha256 = "sha256-UlO3/KvfBbRc6FTFREmhmduQLrXlyJwZh6ufRtgFEK0=";
+  };
+  themeXenlism = pkgs.fetchFromGitHub {
+    owner = "xenlism";
+    repo = "Grub-themes";
+    rev = "40ac048df9aacfc053c515b97fcd24af1a06762f";
+    sha256 = "sha256-ProTKsFocIxWAFbYgQ46A+GVZ7mUHXxZrvdiPJqZJ6I=";
+  };
+in {
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+  # boot.loader.systemd-boot.enable = true;
+  # https://discourse.nixos.org/t/how-to-customize-grub-cfg/59311
   boot.loader.efi.canTouchEfiVariables = true;
-  # boot.loader.timeout = 10;
-  # boot.loader.grub = {
-  #   enable = true;
-  #   device = "nodev";
-  #   efiSupport = true;
-  #   useOSProber = true;
-  # };
+  boot.loader.timeout = 3;
+  boot.loader.grub = {
+    enable = true;
+    device = "nodev";
+    efiSupport = true;
+    useOSProber = true;
+    extraConfig = ''
+      GRUB_DISABLE_OS_PROBER=false
+      GRUB_TIMEOUT_STYLE=menu
+      GRUB_TERMINAL_OUTPUT=gfxterm
+    '';
+    theme = "${themeXenlism}/xenlism-grub-2k-nixos/Xenlism-Nixos";
+  };
 
   # Set your time zone.
   time.timeZone = "Europe/Paris";
