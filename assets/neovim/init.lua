ff1239ead30fb03cfca4443675d1907a1d89c19b
Robin CASSAGNE <robin.jean.cassagne@gmail.com>
2025-08-03 23:15:42 +0200

    :wrench: more fixes on neovim

--- assets/neovim/init.lua
+++ assets/neovim/init.lua
@@ -55,10 +55,6 @@ vim.diagnostic.config({
 	severity_sort = true,
 })
 
--- Show line diagnostics automatically in hover window
-vim.o.updatetime = 250
-vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
-
 -- =======================================================================================
 -- Keymaps
 -- =======================================================================================
